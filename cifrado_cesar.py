dae9afef09bd381c1107afb3b57d48346823255f
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2018-06-29 22:20:03 +0200

    Create cifrado_cesar.py

---
+++ cifrado_cesar.py
@@ -0,0 +1,25 @@
+from VALID import ns, OKI
+import subprocess
+
+while True:
+    texto=input("Tu texto: ")
+
+    if texto==texto.upper(): #PARA MAYUSCULAS
+        abc="ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
+    else:
+        abc="abcdefghijklmnñopqrstuwxyz" #PARA MINUSCULAS
+
+    k=int(input("Valor de desplazamiento: "))
+    cifrad=""
+    
+    for c in texto:
+        if c in abc:
+            cifrad += abc[(abc.index(c)+k)%(len(abc))]
+        else:
+            cifrad+=c
+
+    print("Texto cifrado: ",cifrad)
+    conti=ns(input("¿Desea acontinuar?: "))
+    if conti==("n"):
+        break
+    subprocess.call(["cmd.exe","/C","cls"])
