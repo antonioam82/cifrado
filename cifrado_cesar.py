669a79ff1b6aa2b40af6b8e3e4cbd6a1011399ae
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-02-09 11:04:39 +0100

    Update cifrado_cesar.py

--- cifrado_cesar.py
+++ cifrado_cesar.py
@@ -4,10 +4,7 @@ import subprocess
 while True:
     texto=input("Tu texto: ")
 
-    if texto==texto.upper(): #PARA MAYUSCULAS
-        abc="ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
-    else:
-        abc="abcdefghijklmnñopqrstuwxyz" #PARA MINUSCULAS
+    abc="ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuwxyz" 
 
     k=OKI(input("Valor de desplazamiento: "))
     cifrad=""
@@ -23,3 +20,4 @@ while True:
     if conti==("n"):
         break
     subprocess.call(["cmd.exe","/C","cls"])
+
